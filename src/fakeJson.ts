0f461bdef560e7267eda1fc7f9958429b6fd91c9
Zach Poole <zachpoolework@gmail.com>
2019-06-27 18:31:26 -0500

    added employees to fakeJSON

--- src/fakeJson.ts
+++ src/fakeJson.ts
@@ -14,6 +14,7 @@ const projects = [{
   progress: 100,
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempiquip ex ea commodo consequat. Dutate velit esse cillum dolore eu fugiat nulla pariatur.",
   workers: ["Nick Salter"],
+  link: "",
   startedOn: "2019",
   completedOn: null
 },
@@ -23,10 +24,33 @@ const projects = [{
   progress: 30,
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempiquip ex ea commodo consequat. Dutate velit esse cillum dolore eu fugiat nulla pariatur.",
   workers: ["Sonam Gurung"],
+  link: "",
   startedOn: "2019",
   completedOn: null
 }
 ];
 
 
-export {projects};
+const employees = [
+  {
+    uniqueID: "sadfS12F1t",
+    firstName: "Zach",
+    lastName: "Poole",
+    username: "Zachizpro",
+    email: "zachpoolework@gmail.com",
+    currentProjects: 3,
+    finishedProjects: 10
+  },
+  {
+    uniqueID: "!FFG@H#$hjj2",
+    firstName: "Sonam",
+    lastName: "Gurung",
+    username: "SonamTheMom",
+    email: "sonamgnar@gmail.com",
+    currentProjects: 2,
+    finishedProjects: 15
+  }
+];
+
+
+export {projects, employees};
