38e0003cf3e93b2fd87655e5cc3bfd7f60d91688
Nick Salter <thisislsbdc@gmail.com>
2019-06-27 13:45:51 -0500

    added style to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = ({}) => 
 
-<footer className="page-footer">
+<footer className="page-footer" style={{width: "100%"}}>
 <div className="container">
   <div className="row">
     <div className="col l6 s12">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,12 +19,14 @@ const Wrapper = styled.div`
 const Home = ({userName}:{userName: string})=>
   <Wrapper className="">
       <Navbar/>
+      <div style={{minHeight: "100vh"}}>
       <Card className="bold txt-sm">
       <div className="align-center">Current Projects: 0</div>
       <div className="align-center">Completed Projects: 10</div>
       <div className="align-center">Available Members: <div className="chip">Aayush</div></div>
       <div className="align-center">Average Project/Members: 3.2</div>
       </Card>
+      </div>
       <Footer/>
   </Wrapper>;
 
