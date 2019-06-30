69e8ed11fcf834a1f8d56f25be3b9156cc91af97
Sonam Gurung <gurungamar25@gmail.com>
2019-06-30 16:35:23 -0500

    Changed the front layout as per the grid style

--- src/pages/Startpage.tsx
+++ src/pages/Startpage.tsx
@@ -4,35 +4,42 @@ import './Startpage.css';
 import Logo from '../assets/images/logo_on.png';
 
 const Wrapper = styled.div`
-  background-color: #dddd;
+  background-color: #dde1ea;
   display: grid;
   height: 100vh;
-  grid-template-columns: 1fr;
-  place-items: center;`;
+  grid-template-columns: 29% 42% 29%;
+  place-items: center;
+  `;
 
 const Startpage = ({login, onChange}:{login: any, onChange: any})=>
                   <Wrapper>
+                    <div className="wrapper-item-left"></div>
+                    <div className="wrapper-item-center">
                       <div className="container center" >
-                        <div className="row card ">
-                        <div className="card-image " style={{padding:"2em"}}>
-                            <img src={Logo}/>
-                      </div>
-                        <div className="">
-                          <div className="row card-content">
-                            <div className="input-field col s12">
-                              <input placeholder={"Username or Email"} onChange={onChange}/>
-                              <input placeholder={"Password"} type="password" onChange={onChange}/>
+                          <div className="row card ">
+                          <div className="card-image " style={{padding:"2em"}}>
+                              <img src={Logo}/>
+                        </div>
+                          <div className="">
+                            <div className="row card-content">
+                              <div className="input-field col s12">
+                                <input placeholder={"Username or Email"} onChange={onChange}/>
+                                <input placeholder={"Password"} type="password" onChange={onChange}/>
+                              </div>
+                            </div>
+                            <div className="row">
+                              <div className="col m12">
+                                <p className="center-align">
+                                  <button className="btn btn-small waves-effect waves-light" onClick={login}>Login</button>
+                                </p>
+                              </div>
                             </div>
                           </div>
-                          <div className="row">
-                            <div className="col m12">
-                              <p className="center-align">
-                                <button className="btn btn-small waves-effect waves-light" onClick={login}>Login</button>
-                              </p>
-                            </div>
                           </div>
                         </div>
-                        </div>
-                      </div>
+                    </div>
+                    
+                    <div className="wrapper-item-right"></div>
+                      
                     </Wrapper>;
 export default Startpage;
