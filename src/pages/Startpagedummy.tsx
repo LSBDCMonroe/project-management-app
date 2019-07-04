bb29ce90cda4e2071a987b42d45a19049bdb596a
aayush6194 <aayush6194@gmail.com>
2019-07-04 13:50:00 -0500

    updates, cleaning code

--- src/pages/Startpagedummy.tsx
+++ src/pages/Startpagedummy.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import './Startpage.css';
 import Logo from '../assets/images/logo_on.png';
 //background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%); height: 100vh;
-  
+
 const Wrapper = styled.div`
-  background-color: #dddd;  
+  background-color: #dddd;
   display: grid;
   height: 100vh;
   grid-template-columns: 1fr;
@@ -20,33 +20,30 @@ const Start = styled.form`
 
 
 const Startpage = ({login, onChange}:{login: any, onChange: any})=>
-<Wrapper>
+  <Wrapper>
+    <div className="container center">
 
-                      <div className="container center" >
-                        
-                        <div className="row card ">
-                        <div className="card-image " style={{padding:"2em"}}>
-                            <img src={Logo}/>
-                      </div>
-                        <div className="">
-                          <div className="row card-content">
-                            <div className="input-field col s12">
-                              <input placeholder={"Username or Email"} onChange={onChange}/>
-                              <input placeholder={"Password"} type="password" onChange={onChange}/>
-                            </div>
-                          </div> 
-                          <div className="row">
-                            <div className="col m12">
-                              <p className="center-align">
-                                <button className="btn btn-small waves-effect waves-light" onClick={login}>
-                                  Login
-                                </button>
-                              </p>                                                      
-                            </div>                            
-                          </div>                              
-                        </div>     
-                        </div>
-                      </div>
-                      </Wrapper>;
+        <div className="row card ">
+            <div className="card-image " style={{padding: "2em"}}>
+                <img src={Logo}/>
+            </div>
+            <div className="">
+                <div className="row card-content">
+                    <div className="input-field col s12">
+                        <input placeholder={ "Username or Email"} onChange={onChange}/>
+                        <input placeholder={ "Password"} type="password" onChange={onChange}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col m12">
+                        <p className="center-align">
+                            <button className="btn btn-small waves-effect waves-light" onClick={login}>Login</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </Wrapper>;
 
 export default Startpage;
