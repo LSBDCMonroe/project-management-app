dd3073a3467d4646042b9d45669c1289b356d2ba
Nick Salter <thisislsbdc@gmail.com>
2019-07-08 14:58:23 -0500

    added sign up form content

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,49 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
 import './Startpage.css';
-import { textAlign } from '@material-ui/system';
+
+
 
 const Wrapper = styled.div`
   background-color: #dde1ea;
   display: grid;
   height: 100vh;
-  grid-template-columns: 29% 42% 29%;
   place-items: center;
   `;
 
+  const Header = styled.div`
+  font-size: 250%;
+  font-weight: bolder:
+  text-align: center;
+  padding-bottom: 10%;
+  padding-left: 30%;
+  text-decoration: underline;
+  `;
+
+
 const Signup = ({login, onChange}:{login: any, onChange: any})=>
-                  <Wrapper>
-                    <div className="wrapper-item-left"></div>
-                    <div className="wrapper-item-center">
-                      <div className="container center" >
-                          <div className="row card ">
-                          <div className="card-image " style={{padding:"2em"}}>
-                        </div>
-                        <h2 className="center">Sign Up</h2>
-                          <div className="">
-                            <div className="row card-content">
-                              <div className="input-field col s12">
-                                <input placeholder={"First Name"} onChange={onChange}/>
-                              </div>
-                              <div className="input-field col s12">
-                              <input placeholder={"3x@mpl3P@ssw0rd"} type="password" onChange={onChange}/>
-                              </div>
-                              <div className=" col s12 center">
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col m12">
-                                <p className="center-align">
-                                  <button className="btn btn-small waves-effect waves-light" onClick={login}>Create Account</button>
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                          </div>
-                        </div>
+              <Wrapper className="wrapper-item-center">
+                <div className="row card">
+                  <div className="row card-content">
+                    <p className="center-align">
+                      <Header>SIGN UP</Header>
+                    </p>
+                    <div className="row">
+                      <div className="col S4"><input type="text" placeholder="First Name"/></div>
+                      <div className="col s6"><input type="text" placeholder="Last Name"/></div>
                     </div>
-                    
-                    <div className="wrapper-item-right"></div>
-                      
-                    </Wrapper>;
+                    <div className="row">
+                      <div className="col s12"><input type="text" placeholder="Username"/></div>
+                    </div>
+                    <div className="row">
+                      <div className="col s12"><input type="text" placeholder="Email Address"/></div>
+                    </div>
+                    <div className="row">
+                      <div className="col s12"><input type="text" placeholder="Phone Number (Optional)"/></div>
+                    </div>
+                    <div className="row">
+                      <div className="col s12"><input type="password" placeholder="Password"/></div>
+                    </div>
+                    <div className="row">
+                      <div className="col s12"><input type="password" placeholder="Confirm Password"/></div>
+                    </div>
+                    <p className="center-align">
+                      <button className="btn btn-small waves-effect waves-light" onClick={login}>Create Account</button>
+                    </p>
+                  </div>
+                </div>
+              </Wrapper>;
 export default Signup;
+ 
